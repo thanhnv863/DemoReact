6c590110b9894784776f2cb7c6b396eca214be2e
thanhnv863 <thanh862003hhbg@gmail.com>
2023-06-22 10:27:59 +0700

    state

--- src/views/DemoComponent/MyConponent.js
+++ src/views/DemoComponent/MyConponent.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Thành",
+    nameLove: "Nhung",
+  };
   render() {
-    return <div>Xin Chào Anh Thành Hihi</div>;
+    return (
+      <>
+        <div className="Thanh">Xin Chào Anh {this.state.name} Hihi</div>
+        <div className="Nhung">
+          {this.state.name} love {this.state["nameLove"]}
+        </div>
+      </>
+    );
   }
 }
 
